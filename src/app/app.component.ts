07a8e99f0ceb2c09bf346232f4c6ee40e1874839
mulderf09 <mulderf09@gmail.com>
2020-07-22 14:56:48 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,30 @@ export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   bestMovieEver: boolean = true;
   worseMovieEver: boolean = false;
+
+  selectedIndex: number = -1;
+
+  onVoteClick(index) {
+    if(this.isSelected(index)){
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = index;
+    }
+  }
+
+  isSelected(index) {
+    return this.selectedIndex === index;
+  }
+
+  getButtonText(index) {
+    if(this.isSelected(index)){
+      return "Unvote";
+    } else {
+      return "Vote";
+    }
+  }
+
+  anySelected() {
+    return this.selectedIndex !== -1;
+  }
 }
